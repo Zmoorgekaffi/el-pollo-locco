4bc1ea27b710b7042d9c90105bf16f9a6e911680
andre <andre.karyoti.schuro@gmail.com>
2024-03-02 14:42:36 +0100

    did smartphone optimization | added info button | added control info

--- js/game.js
+++ js/game.js
@@ -8,9 +8,19 @@ function init() {
     keyboard = new Keyboard();
     initLevel();
     world = new World(canvas, keyboard);
-    document.getElementById('start-screen').style = 'display: none;'
+    switchToGameDisplay();
 }
 
 function getFullscreen() {
     canvas.requestFullscreen();
+}
+
+function switchToGameDisplay() {
+    document.getElementById('start-screen').style = 'display: none;';
+    document.getElementById('info-btn').style = 'display: none;';
+}
+
+function toggleInfo() {
+    document.getElementById('info-container').classList.toggle('d-none');
+    document.getElementById('info-btn').classList.toggle('white-border');
 }
