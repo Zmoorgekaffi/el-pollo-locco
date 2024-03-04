24267d92ae465983328d99ebebff59f572b26dc6
andre <andre.karyoti.schuro@gmail.com>
2024-03-04 17:13:42 +0100

    added smartphone controls

--- js/classes/keyboard.class.js
+++ js/classes/keyboard.class.js
@@ -10,59 +10,101 @@ class Keyboard {
 
     constructor() {
         document.addEventListener('keydown', (e) => {
-            if(e.key == 'w') {
+            if (e.key == 'w') {
                 this.KEY_W = true;
             }
 
-            if(e.key == 'a') {
+            if (e.key == 'a') {
                 this.KEY_A = true;
 
                 this.KEY_D = false;
             }
 
-            if(e.key == 's') {
+            if (e.key == 's') {
                 this.KEY_S = true;
             }
 
-            if(e.key == 'd') {
+            if (e.key == 'd') {
                 this.KEY_D = true;
 
                 this.KEY_A = false;
             }
 
-            if(e.key == ' ') {
+            if (e.key == ' ') {
                 this.KEY_SPACE = true;
             }
 
-            if(e.key == '.') {
+            if (e.key == '.') {
                 this.KEY_DOT = true;
             }
         });
 
         document.addEventListener('keyup', (e) => {
-            if(e.key == 'w') {
+            if (e.key == 'w') {
                 this.KEY_W = false;
             }
 
-            if(e.key == 'a') {
+            if (e.key == 'a') {
                 this.KEY_A = false;
             }
 
-            if(e.key == 's') {
+            if (e.key == 's') {
                 this.KEY_S = false;
             }
 
-            if(e.key == 'd') {
+            if (e.key == 'd') {
                 this.KEY_D = false;
             }
 
-            if(e.key == ' ') {
+            if (e.key == ' ') {
                 this.KEY_SPACE = false;
             }
-            
-            if(e.key == '.') {
+
+            if (e.key == '.') {
                 this.KEY_DOT = false;
             }
+        })
+
+        document.getElementById('left').addEventListener('touchstart', (e) => {
+            e.preventDefault();
+            this.KEY_A = true;
+            this.KEY_D = false;
+        });
+
+        document.getElementById('left').addEventListener('touchend', (e) => {
+            e.preventDefault();
+            this.KEY_A = false;
+        })
+
+        document.getElementById('right').addEventListener('touchstart', (e) => {
+            e.preventDefault();
+            this.KEY_D = true;
+            this.KEY_A = false;
+        });
+
+        document.getElementById('right').addEventListener('touchend', (e) => {
+            e.preventDefault();
+            this.KEY_D = false;
+        })
+
+        document.getElementById('jump').addEventListener('touchstart', (e) => {
+            e.preventDefault();
+            this.KEY_SPACE = true;
+        });
+
+        document.getElementById('jump').addEventListener('touchend', (e) => {
+            e.preventDefault();
+            this.KEY_SPACE = false;
+        })
+
+        document.getElementById('throw').addEventListener('touchstart', (e) => {
+            e.preventDefault();
+            this.KEY_DOT = true;
+        });
+
+        document.getElementById('throw').addEventListener('touchend', (e) => {
+            e.preventDefault();
+            this.KEY_DOT = false;
         })
     }
 }
--- js/game.js
+++ js/game.js
@@ -12,15 +12,29 @@ function init() {
 }
 
 function getFullscreen() {
-    canvas.requestFullscreen();
+    document.getElementById('canvasframe').requestFullscreen();
+    document.getElementById('fullscreen-btn').removeEventListener('click', getFullscreen);
+    document.getElementById('fullscreen-btn').addEventListener('click', exitFullscreen);
+}
+
+function exitFullscreen() {
+    document.exitFullscreen();
+    document.getElementById('fullscreen-btn').removeEventListener('click', exitFullscreen);
+    document.getElementById('fullscreen-btn').addEventListener('click', getFullscreen);
 }
 
 function switchToGameDisplay() {
     document.getElementById('start-screen').style = 'display: none;';
-    document.getElementById('info-btn').style = 'display: none;';
+    document.querySelector('.prestart-btns-frame').classList.add('d-none');
 }
 
 function toggleInfo() {
     document.getElementById('info-container').classList.toggle('d-none');
     document.getElementById('info-btn').classList.toggle('white-border');
 }
+
+function toggleMobileControls() {
+    document.getElementById('mobile-controls').classList.toggle('d-none');
+    document.getElementById('mobile-control-btn').classList.toggle('white-border');
+}
+
